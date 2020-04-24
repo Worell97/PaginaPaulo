1e368e4c33f7cde58f2efe0ba6b170fc3465f830
Oscar W. Filho <oscar.filho@eco.centauro>
2020-04-24 16:52:42 -0200

    Cadastrar contato

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -4,73 +4,86 @@ import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
 import LogoImg from '../../assets/Logo-Amarelo.png';
 import api from '../../services/api';
+import Toolbar from './../../components/toolBar/toolBar';
+import SideDrawer from './../../components/SideDrawer/SideDrawer';
+import BackDrop from './../../components/BackDrop/BackDrop';
 
-export default function NewIncident(){
+export default function NewContact(){
     const history = useHistory();
-    const [title, setTitle] = useState();
-    const [description, setDescriptio] = useState();
-    const [value, setValue] = useState();
-    const ongId = localStorage.getItem('ongId')
+    const [name, setName] = useState();
+    const [description, setDescription] = useState();
+    const [number, setNumber] = useState();
+    const [email, setEmail] = useState();
+    const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
 
-    async function handleNewIncident(e){
+    async function handleNewContact(e){
         e.preventDefault();
         const data = {
-            title, 
+            name, 
             description, 
-            value
+            number,
+            email
         };
         try {
-            await api.post('incidents', data, {
-                headers: {
-                    Authorization: ongId,
-                }
+            await api.post('contacts', data, {
             })
-            history.push('/profile');
+            history.push('/');
         } catch (error) {
-            alert('Erro ao cadastrar caso, tente novamente.')
+            alert('Erro ao cadastrar contato, tente novamente.')
         }
 
     }
+    function toggleClickHandler(prevState){
+      setSideDrawerOpen(!prevState.sideDrawerOpen);
+    };
+
+    function backDropClickHandler(){
+      setSideDrawerOpen(false);
+    };
+      let backDrop;
+      if (sideDrawerOpen){
+        backDrop = <BackDrop click={backDropClickHandler}/>
+      };
     return(
         <div className="new-incident-conteiner">
-            <div className="content">
-                <img src={LogoImg} alt="Be The Hero"/>
-                <span>{}</span>
-
-                <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button onClick={handleNewIncident} type="button">
-                    <FiPower size={18} color="#E02041"/> 
-                </button>
+            <div className="content">                    
+                <Toolbar toggleClickHandler={toggleClickHandler}/>
+                <SideDrawer show={sideDrawerOpen}/>
+                {backDrop}
                 <section>
-                    <img src={LogoImg} alt="Be The Hero"/>
-                    <h1>Cadastrar novo caso</h1>
-                    <p>Descreva o caso detalhadamente para encontrar um herói para salvar o dia</p>
+                    <p>Deixe-nos aqui sua mensagem</p>
  
-                    <Link className="back-link" to="/profile">
-                        <FiArrowLeft size={16} color="#E02041"/>
-                        Voltar ao home
+                    <Link className="back-link" to="/">
+                        <FiArrowLeft size={16} color="#ffc20a"/>
+                        Inicio
                     </Link>                   
                 </section>    
-                <form onSubmit={handleNewIncident}>
+                <form onSubmit={handleNewContact}>
                     <input 
-                        placeholder="Titulo do caso"
-                        value={title}
-                        onChange={e => setTitle(e.target.value)}
+                        placeholder="Nome"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
                     
                     />
                     <textarea 
-                        placeholder="Descrição"
+                        placeholder="Mensagem"
                         value={description}
-                        onChange={e => setDescriptio(e.target.value)}
+                        onChange={e => setDescription(e.target.value)}
                     
                     />
                     <input 
-                        placeholder="Valor em reais"
-                        value={value}
-                        onChange={e => setValue(e.target.value)}
+                        placeholder="Telefone"
+                        value={number}
+                        onChange={e => setNumber(e.target.value)}
                     
                     />
-                    <button className="button" type="submit">Cadastrar</button>
+                    <input 
+                        placeholder="Email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    
+                    />
+                    <button className="button" type="submit">Confirmar</button>
                 </form>
             </div>
         </div>
