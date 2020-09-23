642b272277b425525f4f9061259a980cdc05f31d
Oscar W. Filho <oscar.filho@eco.centauro>
2020-09-23 14:53:04 -0300

    correção erro variáveis não usadas

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './styles.css';
 import PageDefault from '../../components/PageDefault';
-import CarouselCustom from '../../components/Carousel';
-import CardBox from '../../components/CardBox';
-export default function Profile(){
-    return(
+export default function Profile() {
+    return (
         <PageDefault>
-            <h1>Produtos</h1>            
-            <CardBox/>
+            <h1>Produtos</h1>
         </PageDefault>
     )
 }
